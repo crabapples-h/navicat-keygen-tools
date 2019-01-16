10a119b483b8594bde94f89a67d274e1365f745f
Double Sine <xiao_ai_yu@live.cn>
2019-01-16 15:00:30 +0800

    Add CapstoneMallocTraits

--- navicat-patcher/ResourceGuardCapstone.hpp
+++ navicat-patcher/ResourceGuardCapstone.hpp
@@ -6,8 +6,17 @@
 struct CapstoneHandleTraits {
     using HandleType = csh;
     static inline const HandleType InvalidValue = 0;
-    static inline void Releasor(csh Handle) {
+    static inline void Releasor(HandleType Handle) {
         cs_close(&Handle);
+    }
+};
+
+template<typename __Type>
+struct CapstoneMallocTraits {
+    using HandleType = __Type*;
+    static inline const HandleType InvalidValue = nullptr;
+    static inline void Releasor(HandleType Handle) {
+        cs_free(Handle, 1);
     }
 };
 
